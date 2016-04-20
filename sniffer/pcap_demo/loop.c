a65555b47ffbe070a461699bf2dcb97e5ce057d3
hexu <hexu_bupt@sina.com>
2016-04-21 01:21:22 +0800

    loop.c add

---
+++ sniffer/pcap_demo/loop.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netinet/if_ether.h>
+
+#include "snf.h"
+
+/* callback function that is passed to pcap_loop(..) and called each time 
+ * a packet is recieved                                                    */
+void my_callback(u_char *useless,const struct pcap_pkthdr* pkthdr,const u_char*
+		packet)
+{
+	static int count = 1;
+	fprintf(stdout,"%d, ",count);
+	if(count == 4)
+		fprintf(stdout,"Come on baby sayyy you love me!!! ");
+	if(count == 7)
+		fprintf(stdout,"Tiiimmmeesss!! ");
+	fflush(stdout);
+	count++;
+}
+
+int main(int argc,char **argv)
+{ 
+	char *dev; 
+	pcap_t* descr;
+
+	if(argc != 2){ fprintf(stdout,"Usage: %s numpackets\n",argv[0]);return 0;}
+
+	/* grab a device to peak into... */
+	dev = Pcap_lookupdev();
+
+	/* open device for reading */
+	descr = Pcap_open_live(dev,BUFSIZ,1,0);
+
+	/* allright here we call pcap_loop(..) and pass in our callback function */
+	/* int pcap_loop(pcap_t *p, int cnt, pcap_handler callback, u_char *user)*/
+	pcap_loop(descr,atoi(argv[1]),my_callback,NULL);
+
+	fprintf(stdout,"\nDone processing packets... wheew!\n");
+	return 0;
+}
+
+
