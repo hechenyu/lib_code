d4c9915a817d86910d51f683144f4974e5f8a74f
hexu <hexu_bupt@sina.com>
2016-08-27 00:20:11 +0800

    change test case

--- list/dlist_test.cpp
+++ list/dlist_test.cpp
@@ -1,64 +1,136 @@
-#include <string>
+#include <cassert>
 #include <iostream>
+#include <string>
+#include <initializer_list>
 #include "dlist.h"
 
-using namespace std;
+#define NAME(list) #list
+
+template <typename T>
+void insert_inorder(Dlist<T> &list, std::initializer_list<T> il)
+{
+    for (auto &val : il) {
+        list_insert_back(list, list_new_node(val));
+    }
+}
+
+template <typename T>
+void insert_reverse(Dlist<T> &list, std::initializer_list<T> il)
+{
+    for (auto &val : il) {
+        list_insert_front(list, list_new_node(val));
+    }
+}
+
+template <typename T>
+void remove_inorder(Dlist<T> &list, const std::string &list_name="list")
+{
+    std::cout << "free " << list_name << ": ";
+    while (!list_is_empty(list)) {
+        auto node = list_remove_front(list);
+        std::cout << node->value << ", ";
+       	list_free_node(node);
+    }
+    std::cout << std::endl;
+}
+
+template <typename T>
+void remove_reverse(Dlist<T> &list, const std::string &list_name="list")
+{
+    std::cout << "free " << list_name << ": ";
+    while (!list_is_empty(list)) {
+        auto node = list_remove_back(list);
+        std::cout << node->value << ", ";
+       	list_free_node(node);
+    }
+    std::cout << std::endl;
+}
+
+template <typename T>
+void print(Dlist<T> &list, const std::string &beg="", const std::string &end="")
+{
+    std::cout << beg;
+    list_for_each(list, [](Dlist_node<T> *node) { std::cout << node->value << ", "; });
+    std::cout << end;
+}
+
+template <typename T>
+bool test_empty(Dlist<T> &list, const std::string &list_name="list")
+{
+    auto is_empty = list_is_empty(list);
+    if (is_empty) {
+        std::cout << list_name << " is empty!";
+    } else {
+        print(list, list_name+": [", "]");
+    }
+    std::cout << std::endl;
+
+    return is_empty;
+}
+
+template <typename T>
+bool test_replace(Dlist<T> &list, T val, T new_val, const std::string &list_name="list")
+{
+    auto x = list_search(list, val);
+    if (x == &list.nil) {
+        std::cout << "not found " << val << " in " << list_name << "!" << std::endl;
+        return false;
+    }
+
+    std::cout << "found " << val << " in " 
+        << list_name << "!" << std::endl;
+    std::cout << "replace " << val << " with " << new_val << std::endl;
+    auto t = list_new_node(new_val);
+    list_insert(x, t);
+    list_remove(x);
+    print(list, "after replace, "+list_name+": [", "]");
+    std::cout << std::endl;
+    return true;
+}
 
 int main(int argc, char *argv[])
 {
-    typedef Dlist<string> List;
-    typedef Dlist_node<string> Node;
+    typedef Dlist<int> List;
+    typedef Dlist_node<int> Node;
 
-    List list;
-    list_init(list);
-    for (int i = 1; i < argc; i++) {
-        if (i % 2) 
-            list_insert_front(list, list_new_node(string(argv[i])));
-        else
-            list_insert_back(list, list_new_node(string(argv[i])));
-    }
+    List list1, list2;
+    list_init(list1);
+    list_init(list2);
 
-    if (list_is_empty(list)) {
-        cout << "list is empty!" << endl;
-    } else {
-        cout << "list: [";
-        list_for_each(list, [](Node *node) { cout << node->value << ", "; });
-        cout << "]" << endl;
-    }
+    std::cout << "---------------------------" << std::endl;
+    std::cout << "after init" << std::endl;
+    bool is_empty;
+    is_empty = test_empty(list1, NAME(list1));
+    assert(is_empty);
+    is_empty = test_empty(list2, NAME(list2));
+    assert(is_empty);
 
-    cout << "entry val for search: ";
-    string val;
-    cin >> val;
-    auto x = list_search(list, val);
-    if (x != &list.nil) {
-        cout << "found it!" << endl;
-		auto t = list_new_node(std::string("fake"));
-		list_insert(x, t);
-		list_remove(x);
-    } else {
-        cout << "not found!" << endl;
-    }
+    insert_inorder(list1, {1, 3, 5, 7, 9});
+    insert_reverse(list2, {0, 2, 4, 6, 8});
 
-    cout << "free list: ";
-	int i = 0;
-    while (!list_is_empty(list)) {
-		Node *node = nullptr;
-		if (i++ % 2) {
-			node = list_remove_front(list);
-		} else {
-			node = list_remove_back(list);
-		}
-		cout << node->value << ", ";
-       	list_free_node(node);
-    }
-	cout << endl;
+    std::cout << "---------------------------" << std::endl;
+    std::cout << "after insert" << std::endl;
+    is_empty = test_empty(list1, NAME(list1));
+    assert(!is_empty);
+    is_empty = test_empty(list2, NAME(list2));
+    assert(!is_empty);
 
-    cout << "after free list" << endl;
-    if (list_is_empty(list)) {
-        cout << "list is empty!" << endl;
-    } else {
-        cout << "list is not empty!" << endl;
-    }
+    std::cout << "---------------------------" << std::endl;
+    std::cout << "test replace" << std::endl;
+    int val = 3;
+    int new_val = 19;
+    bool is_found;
+    is_found = test_replace(list1, val, new_val, NAME(list1));
+    assert(is_found);
+    is_found = test_replace(list2, val, new_val, NAME(list2));
+    assert(!is_found);
+
+    std::cout << "---------------------------" << std::endl;
+    std::cout << "after replace" << std::endl;
+    is_empty = test_empty(list1, NAME(list1));
+    assert(!is_empty);
+    is_empty = test_empty(list2, NAME(list2));
+    assert(!is_empty);
 
 	return 0;
 }
