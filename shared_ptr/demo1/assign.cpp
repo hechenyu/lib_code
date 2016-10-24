dc375af1ec5c48479d44bf8c17b66c8210417eb0
hexu <hexu_bupt@sina.com>
2016-10-24 22:04:00 +0800

    shared_ptr赋值操作demo

---
+++ shared_ptr/demo1/assign.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <stdexcept>
+#include "shared_ptr.h"
+
+using std::cout;
+
+void *operator new(size_t sz)
+{
+    void *res = malloc(sz);
+    if (res == 0) {
+        printf("no memory");
+        throw std::bad_alloc();
+    }
+    printf("\n*** new %d bytes at %p ***\n", (int) sz, res);
+    return res;
+}
+
+void operator delete(void *ptr)
+{
+    printf("\n*** delete at %p ***\n", ptr);
+    free(ptr);
+}
+
+void print_details(const shared_ptr<int> &sp)
+{
+    cout << "use_count: " << sp.use_count() << '\n'
+        << "get: " << sp.get() << '\n';
+    if (sp) {
+        cout << "resource value: " << *sp << '\n';
+    } else {
+        cout << "no resource\n";
+    }
+}
+
+int main()
+{
+    cout << "sizeof int: " << sizeof (int) << '\n';
+    cout << "sizeof sp_counted_impl_p<int>: " << sizeof (sp_counted_impl_p<int>) << '\n';
+
+    cout << "shared_ptr<int> sp0(new int(3));" << '\n';
+    shared_ptr<int> sp0(new int(3));
+
+    cout << "-----------------------------\n";
+    cout << "sp0:\n";
+    print_details(sp0);
+    cout << "-----------------------------\n\n";
+
+    cout << "shared_ptr<int> sp1(sp0);" << '\n';
+    shared_ptr<int> sp1(sp0);
+
+    cout << "-----------------------------\n";
+    cout << "sp0:\n";
+    print_details(sp0);
+    cout << "-----------------------------\n\n";
+
+    cout << "-----------------------------\n";
+    cout << "sp1:\n";
+    print_details(sp1);
+    cout << "-----------------------------\n\n";
+
+    cout << "sp0.reset();" << '\n';
+    sp0.reset();
+
+    cout << "-----------------------------\n";
+    cout << "sp0:\n";
+    print_details(sp0);
+    cout << "-----------------------------\n\n";
+
+    cout << "-----------------------------\n";
+    cout << "sp1:\n";
+    print_details(sp1);
+    cout << "-----------------------------\n\n";
+
+    cout << "sp1.reset();" << '\n';
+    sp1.reset();
+
+    cout << "-----------------------------\n";
+    cout << "sp0:\n";
+    print_details(sp0);
+    cout << "-----------------------------\n\n";
+
+    cout << "-----------------------------\n";
+    cout << "sp1:\n";
+    print_details(sp1);
+    cout << "-----------------------------\n\n";
+
+    return 0;
+}
+
