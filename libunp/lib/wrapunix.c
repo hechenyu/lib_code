a575d6533300aa5e4cfa78bf8e836642f0c34196
hexu <hexu_bupt@sina.com>
2017-03-21 02:54:49 +0800

    merge wrapunix.c from unpv22e

--- libunp/lib/wrapunix.c
+++ libunp/lib/wrapunix.c
@@ -51,24 +51,89 @@ Fattach(int fd, const char *path)
 }
 #endif
 
-int
-Fcntli(int fd, int cmd, int arg)
+int 
+Fcntl(int fd, int cmd, ...)
 {
-	int	n;
+    va_list ap;
+    int iarg;
+    void *varg;
+    int n;
 
-	if ( (n = fcntl(fd, cmd, arg)) == -1)
+    va_start(ap, cmd);
+    if (cmd == F_SETLKW || cmd == F_SETLK) {
+        varg = va_arg(ap, void *);
+        n = fcntl(fd, cmd, varg);
+    } else {
+        iarg = va_arg(ap, int);
+        n = fcntl(fd, cmd, iarg);
+    }
+    va_end(ap);
+
+    if (n == -1) {
 		err_sys("fcntl error");
-	return(n);
+    }
+    return(n);
+}
+
+pid_t
+Fork(void)
+{
+	pid_t	pid;
+
+	if ( (pid = fork()) == -1)
+		err_sys("fork error");
+	return(pid);
+}
+
+long
+Fpathconf(int fd, int name)
+{
+	long	val;
+
+	errno = 0;		/* in case fpathconf() does not change this */
+	if ( (val = fpathconf(fd, name)) == -1) {
+		if (errno != 0)
+			err_sys("fpathconf error");
+		else
+			err_sys("fpathconf: %d not defined", name);
+	}
+	return(val);
+}
+
+void
+Fstat(int fd, struct stat *ptr)
+{
+	if (fstat(fd, ptr) == -1)
+		err_sys("fstat error");
+}
+
+#ifdef	HAVE_SYS_IPC_H
+key_t
+Ftok(const char *pathname, int id)
+{
+	key_t	key;
+
+	if ( (key = ftok(pathname, id)) == -1)
+		err_sys("ftok error for pathname \"%s\" and id %d", pathname, id);
+	return(key);
+}
+#endif	/* HAVE_SYS_IPC_H */
+
+void
+Ftruncate(int fd, off_t length)
+{
+	if (ftruncate(fd, length) == -1)
+		err_sys("ftruncate error");
 }
 
 int
-Fcntlv(int fd, int cmd, void *arg)
+Getopt(int argc, char *const *argv, const char *str)
 {
-	int	n;
+	int		opt;
 
-	if ( (n = fcntl(fd, cmd, arg)) == -1)
-		err_sys("fcntl error");
-	return(n);
+	if ( ( opt = getopt(argc, argv, str)) == '?')
+		exit(1);		/* getopt() has already written to stderr */
+	return(opt);
 }
 
 void
@@ -89,14 +154,21 @@ Ioctl(int fd, int request, void *arg)
 	return(n);	/* streamio of I_LIST returns value */
 }
 
-pid_t
-Fork(void)
+void
+Kill(pid_t pid, int signo)
 {
-	pid_t	pid;
+	if (kill(pid, signo) == -1)
+		err_sys("kill error");
+}
 
-	if ( (pid = fork()) == -1)
-		err_sys("fork error");
-	return(pid);
+off_t
+Lseek(int fd, off_t offset, int whence)
+{
+	off_t	pos;
+
+	if ( (pos = lseek(fd, offset, whence)) == (off_t) -1)
+		err_sys("lseek error");
+	return(pos);
 }
 
 void *
@@ -107,6 +179,20 @@ Malloc(size_t size)
 	if ( (ptr = malloc(size)) == NULL)
 		err_sys("malloc error");
 	return(ptr);
+}
+
+void
+Mkfifo(const char *pathname, mode_t mode)
+{
+	if (mkfifo(pathname, mode) == -1)
+		err_sys("mkfifo error for %s", pathname);
+}
+
+void
+Mktemp(char *template)
+{
+	if (mktemp(template) == NULL || template[0] == 0)
+		err_quit("mktemp error");
 }
 
 int
@@ -126,17 +212,100 @@ Mkstemp(char *template)
 	return i;
 }
 
-#include	<sys/mman.h>
-
+#ifdef	HAVE_SYS_MMAN_H
 void *
 Mmap(void *addr, size_t len, int prot, int flags, int fd, off_t offset)
 {
 	void	*ptr;
 
-	if ( (ptr = mmap(addr, len, prot, flags, fd, offset)) == ((void *) -1))
+	if ( (ptr = mmap(addr, len, prot, flags, fd, offset)) == MAP_FAILED)
 		err_sys("mmap error");
 	return(ptr);
 }
+
+void
+Munmap(void *addr, size_t len)
+{
+	if (munmap(addr, len) == -1)
+		err_sys("munmap error");
+}
+#endif	/* HAVE_SYS_MMAN_H */
+
+#ifdef	HAVE_MQUEUE_H
+mqd_t
+Mq_open(const char *pathname, int oflag, ...)
+{
+	mqd_t	mqd;
+	va_list	ap;
+	mode_t	mode;
+	struct mq_attr	*attr;
+
+	if (oflag & O_CREAT) {
+		va_start(ap, oflag);		/* init ap to final named argument */
+		mode = va_arg(ap, va_mode_t);
+		attr = va_arg(ap, struct mq_attr *);
+		if ( (mqd = mq_open(pathname, oflag, mode, attr)) == (mqd_t) -1)
+			err_sys("mq_open error for %s", pathname);
+		va_end(ap);
+	} else {
+		if ( (mqd = mq_open(pathname, oflag)) == (mqd_t) -1)
+			err_sys("mq_open error for %s", pathname);
+	}
+	return(mqd);
+}
+
+void
+Mq_close(mqd_t mqd)
+{
+	if (mq_close(mqd) == -1)
+		err_sys("mq_close error");
+}
+
+void
+Mq_unlink(const char *pathname)
+{
+	if (mq_unlink(pathname) == -1)
+		err_sys("mq_unlink error");
+}
+
+void
+Mq_send(mqd_t mqd, const char *ptr, size_t len, unsigned int prio)
+{
+	if (mq_send(mqd, ptr, len, prio) == -1)
+		err_sys("mq_send error");
+}
+
+ssize_t
+Mq_receive(mqd_t mqd, char *ptr, size_t len, unsigned int *prio)
+{
+	ssize_t	n;
+
+	if ( (n = mq_receive(mqd, ptr, len, prio)) == -1)
+		err_sys("mq_receive error");
+	return(n);
+}
+
+void
+Mq_notify(mqd_t mqd, const struct sigevent *notification)
+{
+	if (mq_notify(mqd, notification) == -1)
+		err_sys("mq_notify error");
+}
+
+void
+Mq_getattr(mqd_t mqd, struct mq_attr *mqstat)
+{
+	if (mq_getattr(mqd, mqstat) == -1)
+		err_sys("mq_getattr error");
+}
+
+void
+Mq_setattr(mqd_t mqd, const struct mq_attr *mqstat, struct mq_attr *omqstat)
+{
+	if (mq_setattr(mqd, mqstat, omqstat) == -1)
+		err_sys("mq_setattr error");
+}
+#endif	/* HAVE_MQUEUE_H */
 
 int
 Open(const char *pathname, int oflag, mode_t mode)
--- libunp/lib/wrapunix.h
+++ libunp/lib/wrapunix.h
@@ -3,33 +3,88 @@
 
 #include <unistd.h>
 #include <fcntl.h>
-#include <sys/time.h>
 #include <signal.h>
+#include <stdarg.h>
+#include <mqueue.h>
+#include <sys/time.h>
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <sys/sysctl.h>
 #include <sys/ioctl.h>
+#include <sys/ipc.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
 #include "error.h"
 
 #define HAVE_MKSTEMP 1
+#define HAVE_SYS_IPC_H 1
+#define HAVE_SYS_MMAN_H 1
+#define HAVE_MQUEUE_H 1
+
+/*
+ * In our wrappers for open(), mq_open(), and sem_open() we handle the
+ * optional arguments using the va_XXX() macros.  But one of the optional
+ * arguments is of type "mode_t" and this breaks under BSD/OS because it
+ * uses a 16-bit integer for this datatype.  But when our wrapper function
+ * is called, the compiler expands the 16-bit short integer to a 32-bit
+ * integer.  This breaks our call to va_arg().  All we can do is the
+ * following hack.  Other systems in addition to BSD/OS might have this
+ * problem too ...
+ */
+
+#ifdef  __bsdi__
+#define va_mode_t   int
+#else
+#define va_mode_t   mode_t
+#endif
+/* $$.ix [va_mode_t]~datatype,~definition~of$$ */
 
 			/* prototypes for our Unix wrapper functions: see {Sec errors} */
 void	*Calloc(size_t, size_t);
 void	 Close(int);
 void	 Dup2(int, int);
+
 #ifdef	HAVE_FATTACH
 void     Fattach(int fd, const char *path);
 #endif
-int		 Fcntli(int, int, int);
-int		 Fcntlv(int, int, void *);
-#define Fcntl(fd, cmd, arg) \
-    ((((cmd) == F_SETLKW) || ((cmd) == F_SETLK)) ? Fcntlv((fd), (cmd), (void *) (arg)) : Fcntli((fd), (cmd), (int) (arg)))
+
+int      Fcntl(int, int, ...);
+pid_t	 Fork(void);
+long     Fpathconf(int fd, int name);
+void     Fstat(int fd, struct stat *ptr);
+
+#ifdef	HAVE_SYS_IPC_H
+key_t    Ftok(const char *pathname, int id);
+#endif
+
+void     Ftruncate(int fd, off_t length);
+int      Getopt(int argc, char *const *argv, const char *str);
+
 void	 Gettimeofday(struct timeval *, void *);
 int		 Ioctl(int, int, void *);
-pid_t	 Fork(void);
+void     Kill(pid_t pid, int signo);
+off_t    Lseek(int fd, off_t offset, int whence);
 void	*Malloc(size_t);
+void     Mkfifo(const char *pathname, mode_t mode);
+void     Mktemp(char *template);
 int	     Mkstemp(char *);
+
+#ifdef	HAVE_SYS_MMAN_H
 void	*Mmap(void *, size_t, int, int, int, off_t);
+void     Munmap(void *addr, size_t len);
+#endif
+
+#ifdef	HAVE_MQUEUE_H
+mqd_t    Mq_open(const char *pathname, int oflag, ...);
+void     Mq_close(mqd_t mqd);
+void     Mq_unlink(const char *pathname);
+void     Mq_send(mqd_t mqd, const char *ptr, size_t len, unsigned int prio);
+ssize_t  Mq_receive(mqd_t mqd, char *ptr, size_t len, unsigned int *prio);
+void     Mq_notify(mqd_t mqd, const struct sigevent *notification);
+void     Mq_getattr(mqd_t mqd, struct mq_attr *mqstat);
+void     Mq_setattr(mqd_t mqd, const struct mq_attr *mqstat, struct mq_attr *omqstat);
+#endif
+
 int		 Open(const char *, int, mode_t);
 void	 Pipe(int *fds);
 ssize_t	 Read(int, void *, size_t);
