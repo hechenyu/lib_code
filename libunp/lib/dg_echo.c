25db315597540a74706e5ed15193b4a46589d022
hexu <hexu_bupt@sina.com>
2017-04-15 01:19:30 +0800

    add unixdomain

--- libunp/lib/dg_echo.c
+++ libunp/lib/dg_echo.c
@@ -7,9 +7,19 @@ dg_echo(int sockfd, SA *pcliaddr, socklen_t clilen)
 	socklen_t	len;
 	char		mesg[MAXLINE];
 
+#ifdef	NOTDEF
+    char                str[INET_ADDRSTRLEN];
+#endif
+
 	for ( ; ; ) {
 		len = clilen;
 		n = Recvfrom(sockfd, mesg, MAXLINE, 0, pcliaddr, &len);
+
+#ifdef	NOTDEF
+			printf("new client: %s, port %d\n",
+					Inet_ntop(AF_INET, &pcliaddr->sin_addr, str, sizeof(str)),
+					ntohs(pcliaddr->sin_port));
+#endif
 
 		Sendto(sockfd, mesg, n, 0, pcliaddr, len);
 	}
