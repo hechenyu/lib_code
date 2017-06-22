8a3f90fb9019cdedf1fa145e6f59007fe628c363
hexu <hexu_bupt@sina.com>
2017-06-23 00:31:58 +0800

    add recv_rountine

--- libunp/lib/udp_connect.h
+++ libunp/lib/udp_connect.h
@@ -7,7 +7,15 @@
 #include "error.h"
 #include "wrapunix.h"
 
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
 int udp_connect(const char *, const char *);
 int Udp_connect(const char *, const char *);
+
+#ifdef  __cplusplus
+}   // extern "C"
+#endif
 
 #endif
--- libunp/tools/udp_stress_client/main.cpp
+++ libunp/tools/udp_stress_client/main.cpp
@@ -3,6 +3,7 @@
 #include <chrono>
 #include "prog_opts_util.h"
 #include "udp_connect.h"
+#include "wrapsock.h"
 
 using namespace std;
 
@@ -12,6 +13,14 @@ struct Send_conf {
     int sleep_per_loop;
     std::atomic<int> total_send_packets;
 };
+
+struct Recv_conf {
+    int bytes_per_packet;
+    std::atomic<int> total_recv_packets;
+};
+
+static void send_rountine(vector<int> fd_set, Send_conf *send_conf);
+static void recv_rountine(vector<int> fd_set, Recv_conf *recv_conf);
 
 int main(int argc, char *argv[])
 {
@@ -32,6 +41,16 @@ int main(int argc, char *argv[])
     send_conf.packets_per_loop = vm["packets_per_loop"].as<int>();
     send_conf.sleep_per_loop = vm["sleep_per_loop"].as<int>();
     send_conf.total_send_packets = 0;
+
+    Recv_conf recv_conf;
+    recv_conf.bytes_per_packet = vm["bytes_per_packet"].as<int>();
+    recv_conf.total_recv_packets = 0;
+
+    std::thread send_thread(send_rountine, fd_set, &send_conf);
+    send_thread.detach();
+
+    std::thread recv_thread(recv_rountine, fd_set, &recv_conf);
+    recv_thread.detach();
 
     return 0;
 }
@@ -56,5 +75,39 @@ void send_rountine(vector<int> fd_set, Send_conf *send_conf)
         }
 
         this_thread::sleep_for(chrono::microseconds(sleep_per_loop));
+    }
+}
+
+void recv_rountine(vector<int> fd_set, Recv_conf *recv_conf)
+{
+    const int MAX_EVENTS = 5;
+    int     bytes_per_packet = recv_conf->bytes_per_packet; 
+    char    *buff = new char[bytes_per_packet+1]; 
+
+    int    i, epfd, nready, sockfd;
+    struct epoll_event  ev;
+    struct epoll_event  evlist[MAX_EVENTS];
+
+    epfd = Epoll_create(MAX_EVENTS);
+
+    int nclient = fd_set.size();
+    for (int i = 0; i < nclient; i++) {
+        sockfd = fd_set[i];
+        ev.data.fd = sockfd;
+        ev.events = EPOLLIN; /* Only interested in input events */
+        Epoll_ctl(epfd, EPOLL_CTL_ADD, sockfd, &ev);
+    }
+
+	for ( ; ; ) {
+		nready = Epoll_wait(epfd, evlist, MAX_EVENTS, -1);
+        for (i = 0; i < nready; i++) {
+            if (evlist[i].events & EPOLLIN) {  /* net data in */
+                sockfd = evlist[i].data.fd;
+                if (bytes_per_packet == recv(sockfd, buff, bytes_per_packet, MSG_DONTWAIT)) {
+                    recv_conf->total_recv_packets++;
+                }
+                continue;
+            }
+        }
     }
 }
